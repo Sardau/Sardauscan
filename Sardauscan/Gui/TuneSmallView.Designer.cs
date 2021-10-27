8918b9025cfb7114fdffad037466e3041473a5cb
git <fabio@ferretti.info>
2021-10-27 08:05:35 +0200

    copyright
    changing copyright to CC-BY-NC-SA

--- Sardauscan/Gui/TuneSmallView.Designer.cs
+++ Sardauscan/Gui/TuneSmallView.Designer.cs
@@ -1,25 +1,8 @@
 ﻿#region COPYRIGHT
-/****************************************************************************
- *  Copyright (c) 2015 Fabio Ferretti <https://plus.google.com/+FabioFerretti3D>                 *
- *  This file is part of Sardauscan.                                        *
- *                                                                          *
- *  Sardauscan is free software: you can redistribute it and/or modify      *
- *  it under the terms of the GNU General Public License as published by    *
- *  the Free Software Foundation, either version 3 of the License, or       *
- *  (at your option) any later version.                                     *
- *                                                                          *
- *  Sardauscan is distributed in the hope that it will be useful,           *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
- *  GNU General Public License for more details.                            *
- *                                                                          *
- *  You are not allowed to Sell in any form this code                       * 
- *  or any compiled version. This code is free and for free purpose only    *
- *                                                                          *
- *  You should have received a copy of the GNU General Public License       *
- *  along with Sardaukar.  If not, see <http://www.gnu.org/licenses/>       *
- ****************************************************************************
-*/
+/**
+ * This file is part of Sardauscan by Fabio Ferretti, licensed under the CC-BY-NC-SA 4.0 Licence.
+ * You can find the original code in this GitHub repository: https://github.com/Sardau/Sardauscan
+ */
 #endregion
 using Sardauscan.Gui.Controls;
 namespace Sardauscan.Gui
